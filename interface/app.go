37c899e44e992faebe92e26c648af5d5323f5dde
viper6z <oskarbhc@gmail.com>
2026-07-13 01:24:51 +0200

    interface work

--- interface/app.go
+++ interface/app.go
@@ -4,6 +4,9 @@ import (
 	"go.yaml.in/yaml/v4"
 	"io"
 	"os"
+	"fmt"
+	"errors"
+	"strings"
 )
 
 func main() {
@@ -17,19 +20,11 @@ func main() {
 		os.Stderr.WriteString("Invalid input, allowed subcommands are: validate\n")
 		os.Exit(2)
 	}
-	
-	result, err := openApplicationFile(os.Args[2])
-		if err != nil {
-		os.Stderr.WriteString("Invalid path\n")
-		os.Exit(2)
-	}
-	defer result.Close() //this waits until main is done to close the file
-	
-	_, err = decodeApplicationYAML(result)
 
+	err := loadApplication(os.Args[2])
 	if err != nil {
-		os.Stderr.WriteString("error decoding yaml manifest")
-		os.Exit(2)
+		fmt.Fprintf(os.Stderr, "Failed to load application: %v\n", err)
+		os.Exit(1)
 	}
 }
 
@@ -40,6 +35,24 @@ type Application struct {
 	InternalPort        int      `yaml:"internal_port"`
 	ExposureType        string   `yaml:"exposure_type"`
 	AllowedPublicRoutes []string `yaml:"allowed_public_routes"`
+}
+
+//loadApplication opens, decodes, and closes an application manifest.
+func loadApplication (path string) (error) {
+	manifest, err := openApplicationFile(path)
+	if err != nil {
+		return err
+	}
+	defer manifest.Close()
+	application, err := decodeApplicationYAML(manifest)
+	if err != nil {
+		return err
+	}
+	err = validateApplication(application)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // receive path from arg
@@ -66,4 +79,34 @@ func decodeApplicationYAML(reader io.Reader) (Application, error) {
 	}
 
 	return application, err
+}
+
+//domain validation
+func validateApplication(application Application) (error) {
+	if application.Name == "" {
+		return errors.New("Name can't be empty")
+	}
+
+	}
+	if application.Image == "" || !strings.HasPrefix(application.Image, "ghcr.io") {
+		return errors.New("Image needs to be non empty and in viper6z ghcr")
+	}
+	
+	if application.InternalPort < 1 || application.InternalPort > 65535 {
+		return errors.New("internal_port must be a number between 1 and 65535")
+	}
+
+	if application.ExposureType == "" || application.ExposureType != "public" && application.ExposureType != "internal" {
+		return errors.New("exposure type must not be empty, exposure type is either public or internal")
+	}
+	
+	if application.ExposureType == "internal" && len(application.AllowedPublicRoutes) > 0 {
+		return errors.New("internal apps may not have public routes")
+	}
+
+	if application.ExposureType == "public" && len(application.AllowedPublicRoutes) == 0 {
+		return errors.New("public apps need atleast 1 public route")
+	}
+	
+	return nil
 }
